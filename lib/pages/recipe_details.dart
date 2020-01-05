9644b60d76356db32cac2798ab381f79463074c4
IsnantoBudi <45707679+IsnantoBudi@users.noreply.github.com>
2020-01-06 01:45:06 +0700

    tambahan recipe

--- lib/pages/recipe_details.dart
+++ lib/pages/recipe_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:masak_kuy/Hyperlink/Hyperlink.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class RecipeDetails extends StatelessWidget {
 
@@ -8,53 +10,10 @@ class RecipeDetails extends StatelessWidget {
   RecipeDetails(this.post);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Resep')),
-        body: StreamBuilder(
-            stream:Firestore.instance.collection('post').snapshots(),
-            builder:(context,snapshot){
-              if(!snapshot.hasData){
-                const Text('Loading');
-              }
-              else{
-                return ListView.builder(
-                    itemCount: snapshot.data.documents.length,
-                    itemBuilder: (context,index){
-                      DocumentSnapshot mypost=snapshot.data.documents[index];
-                      return Stack(
-                        children: <Widget>[
-                          Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 350.0,
-                            child:Padding(
-                              padding: EdgeInsets.only(top:8.0,bottom:8.0),
-                              child:Material(
-                                color: Colors.white,
-                                elevation : 14.0,
-                                shadowColor: Color(0x802196f3),
-                                child: Center(child: Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: Column(children: <Widget>[
-                                    Container(
-                                      width: MediaQuery.of(context).size.width,
-                                      height:200.0,
-                                      child: Image.network(
-                                          '${mypost['gambar']}',
-                                          fit :BoxFit.fill
-                                      ),
-                                    ),
-                                  ],),
-                                ),),
-                              ),
-                            ),
-                          ),
-                        ],
-                      );}
-                );
-              }
-            }
-        )
+    return WebviewScaffold(
+      url: 'https://www.dapurumami.com/resep/caesar-salad-ala-mayumi-n6gOE',
+      hidden: true,
+      appBar: AppBar(title: const Text("Recipe")),
     );
   }
 }
